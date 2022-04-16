ca0e981752b547cfb4b870a94fad7fd705187b00
ivan bartolelli <ivanbartolelli@gmail.com>
2022-04-16 17:49:35 -0300

    firestore emulator setup and firts function for push

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ const functions = require('firebase-functions');
 const express = require('express')
 const app = express()
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
 const mercadopago = require('mercadopago');
 mercadopago.configure({
     access_token: 'TEST-1629463471863995-080922-f03a5bedefc55807047b9211bebcc4bc-181705181'
@@ -68,6 +72,26 @@ app.post('/api/paymentReference', (req, res) => {
 
 
 })
+
+
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+      // Grab the current value of what was written to Firestore.
+      const original = snap.data().original;
+
+      // Access the parameter `{documentId}` with `context.params`
+      functions.logger.log('Uppercasing', context.params.documentId, original);
+
+      const uppercase = original.toUpperCase();
+
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to Firestore.
+      // Setting an 'uppercase' field in Firestore document returns a Promise.
+      return snap.ref.set({uppercase}, {merge: true});
+    });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
